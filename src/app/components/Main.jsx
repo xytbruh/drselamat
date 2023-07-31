d337deff3aaecff7e6ed83637404e28c80b7660e
Dimas Nugraha <127030192+xytbruh@users.noreply.github.com>
2023-07-31 14:08:15 +0700

    add

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { Carousel } from "react-bootstrap";
 
 export default function Main() {
