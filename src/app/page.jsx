5183243303cda4749b89342ee1d99d20c1d234a8
Dimas Nugraha <127030192+xytbruh@users.noreply.github.com>
2023-07-31 16:07:20 +0700

    add

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,14 @@
 "use client";
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 import Header from "./components/Navbar";
 import { Analytics } from "@vercel/analytics/react";
 import ReactGA from "react-ga";
-const TRACKING_ID = "G-0KZ9ZNH0HC"; // YOUR_OWN_TRACKING_ID
-ReactGA.initialize(TRACKING_ID);
 
 export default function Home() {
   useEffect(() => {
+    ReactGA.initialize("G-GCJPPC7XPK");
     // Track pageview when the component is loaded
     ReactGA.pageview(window.location.pathname + window.location.search);
   }, []);
