d5e7b1eea74a7a02e88fea043604da2869465817
Dimas Nugraha <127030192+xytbruh@users.noreply.github.com>
2023-07-31 14:07:04 +0700

    add

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,10 +2,12 @@
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 import Header from "./components/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   return (
     <>
+      <Analytics />
       <Header />
       <Main />
       <Footer />
